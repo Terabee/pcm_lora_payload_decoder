b06efb16e75be21302f2ac3ede8c1bf71e4b786c
Shaibi Achraf <achraf.shaibi@terabee.com>
2023-11-22 09:01:06 +0100

    feat: Add support to downlink responses

--- javascript/decoder.js
+++ javascript/decoder.js
@@ -2,6 +2,19 @@ function isKthBitSet(byte, k) {
   return byte & (1 << k);
 }
 
+function uint16(bytes) {
+  if (bytes.length !== uint16.BYTES) {
+    throw new Error('uint16 must have exactly 2 bytes');
+  }
+
+  var integer = 0;
+  for (var x = 0; x < bytes.length; x++) {
+    integer += integer*255 + bytes[x]
+  }
+  return integer;
+};
+uint16.BYTES = 2;
+
 function uint32(bytes) {
   if (bytes.length !== uint32.BYTES) {
     throw new Error('uint32 must have exactly 4 bytes');
@@ -38,6 +51,109 @@ function parseCounts(payload){
   return data
 }
 
+function parseMountingHeight(payload){
+  data = {
+    mounting_height: uint16(payload.slice(0, 2))
+  }
+  return data
+}
+
+function parsePushPeriod(payload){
+  data = {
+    push_period_min: uint16(payload.slice(0, 2))
+  }
+  return data
+}
+
+function parseCountDirection(payload){
+  data = {
+    direction: (payload.slice(0) == 1 ? "IN" : "OUT")
+  }
+  return data
+}
+
+function parseCablePosition(payload){
+  data = {
+    cable_position: (payload.slice(0) == 1 ? "LEFT" : "RIGHT")
+  }
+  return data
+}
+
+function parseAccessPointState(payload){
+  data = {
+    state: (payload.slice(0) == 1 ? "ENABLED" : "DISABLED")
+  }
+  return data
+}
+
+function parseSoftwareVersion(payload){
+  data = {
+    software_version: String.fromCharCode(...payload.slice(1, 11)).slice(0, -1)
+  }
+  return data
+}
+
+function parseHeader(bytes) {
+  header = {}
+
+  if (bytes[0] === 255){
+    header.cmd_id = bytes[1]
+    header.ack = (bytes[2] === 255 ? false : true)
+    header.type = "acknowledge"
+  } else {
+    header.cmd_id = bytes[0]
+    header.ack = true
+    header.type = "response"
+  }
+
+  return header
+}
+
+const commands = new Map()
+
+function registerCommand(
+  registred_commands_map,
+  fport, command_name, cmd_id,
+  parsePayload = undefined
+) {
+
+  if (fport < 1 || fport > 255){
+    throw "fport must be between 1 and 255"
+  }
+
+  if (cmd_id < 0 || cmd_id > 254){
+    throw "cmd_id must be between 0 and 254"
+  }
+
+  fport_hex = fport.toString(16).padStart(2, '0');
+  cmd_id_hex = cmd_id.toString(16).padStart(2, '0');
+
+  key = fport_hex + cmd_id_hex
+
+  registred_commands_map.set(key, {
+    command_name: command_name,
+    parsePayload: parsePayload
+  })
+}
+
+function getCommand(
+  registred_commands_map, fport, cmd_id
+) {
+  fport_hex = fport.toString(16).padStart(2, '0');
+  cmd_id_hex = cmd_id.toString(16).padStart(2, '0');
+
+  key = fport_hex + cmd_id_hex
+  command = registred_commands_map.get(key)
+
+  if (!command){
+    throw "command not registered"
+  }
+
+  return command
+}
+
+
+
 
 /* The Things Network Payload Decoder
   Converts raw bytes to ouptut object
@@ -69,6 +185,62 @@ COUNTING_DATA_UPLINK = 1
 /* UPLINKS WITH CUSTOM FRAME STRUCTURE END */
 
 
+F_PORT_COUNTS = 2
+/* HANDLER GROUP COUNTS COMMANDS */
+registerCommand(commands, F_PORT_COUNTS, "CMD_CNT_RST", 1)
+registerCommand(commands, F_PORT_COUNTS, "CMD_CNT_GET", 2,
+  parsePayload = parseCounts
+)
+registerCommand(commands, F_PORT_COUNTS, "CMD_CNT_SET", 130)
+/* HANDLER GROUP COUNTS END */
+
+F_PORT_REBOOT = 3
+/* HANDLER GROUP REBOOT COMMANDS */
+registerCommand(commands, F_PORT_REBOOT, "CMD_DEV_RBT", 1)
+registerCommand(commands, F_PORT_REBOOT, "CMD_TPC_RST", 2)
+/* HANDLER GROUP REBOOT END */
+
+F_PORT_GET_SOFTWARE_VERSION = 4
+/* HANDLER GROUP GET SOFTWARE VERSION COMMANDS */
+registerCommand(commands, F_PORT_GET_SOFTWARE_VERSION, "CMD_GET_SW_VER", 1,
+  parsePayload = parseSoftwareVersion
+)
+/* HANDLER GROUP GET SOFTWARE VERSION END */
+
+F_PORT_ACCESS_POINT = 5
+/* HANDLER GROUP ACCESS POINT COMMANDS */
+registerCommand(commands, F_PORT_ACCESS_POINT, "CMD_GET_AP_STATE", 1,
+  parsePayload = parseAccessPointState
+)
+registerCommand(commands, F_PORT_ACCESS_POINT, "CMD_SET_AP_STATE", 129)
+/* HANDLER GROUP ACCESS POINT END */
+
+F_PORT_REJOIN = 6
+/* HANDLER GROUP REJOIN COMMANDS */
+registerCommand(commands, F_PORT_REJOIN, "CMD_FORCE_REJOIN", 1)
+/* HANDLER GROUP REJOIN END */
+
+F_PORT_COUNTING_PARAM = 7
+/* HANDLER GROUP COUNTING PARAMETERS COMMANDS */
+registerCommand(commands, F_PORT_COUNTING_PARAM, "CMD_SET_HEIGHT", 129)
+registerCommand(commands, F_PORT_COUNTING_PARAM, "CMD_GET_HEIGHT", 1,
+  parsePayload = parseMountingHeight
+)
+
+registerCommand(commands, F_PORT_COUNTING_PARAM, "CMD_SET_COUNTING_DIRECTION", 130)
+registerCommand(commands, F_PORT_COUNTING_PARAM, "CMD_GET_COUNTING_DIRECTION", 2,
+  parsePayload = parseCountDirection
+)
+registerCommand(commands, F_PORT_COUNTING_PARAM, "CMD_SET_PUSH_PERIOD", 131)
+registerCommand(commands, F_PORT_COUNTING_PARAM, "CMD_GET_PUSH_PERIOD", 3,
+  parsePayload = parsePushPeriod
+)
+registerCommand(commands, F_PORT_COUNTING_PARAM, "CMD_SET_CABLE_CONNECTION", 132)
+registerCommand(commands, F_PORT_COUNTING_PARAM, "CMD_GET_CABLE_CONNECTION", 4,
+  parsePayload = parseCablePosition
+)
+/* HANDLER GROUP COUNTING PARAMETERS END */
+
 function decodeUplink(input) {
 
   var data = new Object()
@@ -84,11 +256,34 @@ function decodeUplink(input) {
       data
     };
   }
-  else{
+
+  var header = parseHeader(input.bytes)
+
+  var command
+  try {
+    command = getCommand(commands, fport, header.cmd_id)
+  } catch (e) {
     return {
-      "errors": ["unknown command"]
+      errors: ["unknown command"]
     }
   }
+
+  data.cmd = {
+      name: command.command_name,
+      id: header.cmd_id,
+      success: header.ack
+  }
+
+  if (header.type === "response") {
+    payload = input.bytes.slice(1)
+
+    data.cmd.value = command.parsePayload(payload)
+  }
+
+  return {
+    data
+  };
+
 }
 
 
@@ -99,5 +294,7 @@ module.exports = {
   uint32,
   decodeUplink,
   parseCounts,
+  registerCommand,
+  getCommand
 };
 
--- javascript/decoder.test.js
+++ javascript/decoder.test.js
@@ -2,7 +2,10 @@ const { expect, test } = require("@jest/globals");
 const {
   isKthBitSet,
   uint32,
-  decodeUplink
+  decodeUplink,
+  registerCommand,
+  getCommand,
+  parseCounts
 } = require('./decoder');
 
 test('test passes', () => {
@@ -78,6 +81,74 @@ describe('uint32', () => {
 
   it("should fail with too little bytes error", () => {
     expect(() => uint32([0, 2, 2])).toThrow('uint32 must have exactly 4 bytes')
+  })
+})
+
+describe('registerCommand', () => {
+  it("should register command", () => {
+    const registered_commands_map = new Map()
+
+    fport = 2
+    registerCommand(registered_commands_map, fport, "CMD_CNT_RST", 1)
+
+    expect(registered_commands_map.get("0201")).toMatchObject({
+      command_name: "CMD_CNT_RST"
+    })
+
+    registerCommand(registered_commands_map, fport, "CMD_CNT_SET", 130)
+
+    expect(registered_commands_map.get("0282")).toMatchObject({
+      command_name: "CMD_CNT_SET"
+    })
+  })
+
+  it("should fail with fport out of bounds", () => {
+    const registered_commands_map = new Map()
+
+    fport = 256
+    expect(() => registerCommand(
+      registered_commands_map, fport, "CMD_ID_FOO", 200
+      ).toThrow("fport must be between 1 and 255")
+    )
+  })
+
+  it("should fail with cmd_id out of bounds", () => {
+    const registered_commands_map = new Map()
+
+    fport = 30
+    expect(() => registerCommand(fport, "CMD_ID_WRONG", 255
+      ).toThrow("cmd_id must be between 0 and 254")
+    )
+  })
+
+  it("should get handler name and command name", () => {
+    const registered_commands_map = new Map()
+
+    fport = 2
+    registerCommand(registered_commands_map, fport, "CMD_CNT_RST", 1)
+
+    expect(getCommand(registered_commands_map, 2 , 1)).toMatchObject({
+      command_name: "CMD_CNT_RST"
+    })
+  })
+
+  it("should fail with command not registered", () => {
+    const registered_commands_map = new Map()
+
+    expect(() => getCommand(registered_commands_map, 10, 10)).toThrow("command not registered")
+  })
+
+  it("should execute the foo command payload parser", () => {
+    const registered_commands_map = new Map()
+
+    const fooParser = function(payload) {
+      return payload
+    }
+    fport = 99
+    registerCommand(registered_commands_map, fport, "CMD_FOO", 99, parsePayload = fooParser)
+
+    command = getCommand(registered_commands_map, 99, 99)
+    expect(command.parsePayload([255, 255])).toStrictEqual([255, 255])
   })
 })
 
@@ -171,6 +242,222 @@ describe('decodeUplink', () => {
         }
       }
     )
+  })
+
+  it('should handle CMD_CNT_GET command', () => {
+
+    fport = 2
+
+    const input = {
+      fPort: fport,
+      bytes: [2, 0, 0, 0, 200, 0, 0, 1, 200]
+    }
+
+    expect(decodeUplink(input))
+    .toMatchObject({data: {
+      cmd: {
+        name: "CMD_CNT_GET",
+        id: 2,
+        success: true,
+        value: {
+          count_in: 200,
+          count_out: 456
+        }
+      }
+    }
+
+    })
+  })
+
+
+  it('should handle CMD_GET_AP_STATE command', () => {
+    const registered_commands_map = new Map()
+
+    fport = 5
+
+    let input = {
+      fPort: fport,
+      bytes: [1, 0]
+    }
+
+    expect(decodeUplink(input))
+    .toMatchObject({data: {
+      cmd: {
+        name: "CMD_GET_AP_STATE",
+        id: 1,
+        success: true,
+        value: {
+          state: "DISABLED"
+        }
+      }
+    }})
+
+    input = {
+      fPort: fport,
+      bytes: [1, 1]
+    }
+
+    expect(decodeUplink(input))
+    .toMatchObject({data: {
+      cmd: {
+        name: "CMD_GET_AP_STATE",
+        id: 1,
+        success: true,
+        value: {
+          state: "ENABLED"
+        }
+      }
+    }})
+  })
+
+  it('should handle CMD_GET_SW_VER command', () => {
+
+    fport = 4
+
+    let input = {
+      fPort: fport,
+      bytes: [1, 0, 118, 48, 46, 57, 46, 50, 45, 69, 85, 0]
+    }
+
+    expect(decodeUplink(input))
+    .toMatchObject({data: {
+      cmd: {
+        name: "CMD_GET_SW_VER",
+        id: 1,
+        success: true,
+        value: {
+          software_version: "v0.9.2-EU"
+        }
+      }
+    }})
+  })
+
+  it('should handle CMD_GET_HEIGHT command', () => {
+    const registered_commands_map = new Map()
+
+    fport = 7
+
+    const input = {
+      fPort: fport,
+      bytes: [1, 5, 220]
+    }
+
+    expect(decodeUplink(input))
+    .toMatchObject({data: {
+      cmd: {
+        name: "CMD_GET_HEIGHT",
+        id: 1,
+        success: true,
+        value: {
+          mounting_height: 1500
+        }
+      }
+    }})
+  })
+
+  it('should handle CMD_GET_COUNTING_DIRECTION command', () => {
+    const registered_commands_map = new Map()
+
+    fport = 7
+
+    let input = {
+      fPort: fport,
+      bytes: [2, 1]
+    }
+
+    expect(decodeUplink(input))
+    .toMatchObject({data: {
+      cmd: {
+        name: "CMD_GET_COUNTING_DIRECTION",
+        id: 2,
+        success: true,
+        value: {
+          direction: "IN"
+        }
+      }
+    }})
+
+    input = {
+      fPort: fport,
+      bytes: [2, 0]
+    }
+
+    expect(decodeUplink(input))
+    .toMatchObject({data: {
+      cmd: {
+        name: "CMD_GET_COUNTING_DIRECTION",
+        id: 2,
+        success: true,
+        value: {
+          direction: "OUT"
+        }
+      }
+    }})
+  })
+
+
+  it('should handle CMD_GET_PUSH_PERIOD command', () => {
+    const registered_commands_map = new Map()
+
+    fport = 7
+
+    const input = {
+      fPort: fport,
+      bytes: [3, 0, 60]
+    }
+
+    expect(decodeUplink(input))
+    .toMatchObject({data: {
+      cmd: {
+        name: "CMD_GET_PUSH_PERIOD",
+        id: 3,
+        success: true,
+        value: {
+          push_period_min: 60
+        }
+      }
+    }})
+  })
+
+  it('should handle CMD_GET_CABLE_CONNECTION command', () => {
+    const registered_commands_map = new Map()
+
+    fport = 7
+
+    let input = {
+      fPort: fport,
+      bytes: [4, 1]
+    }
+
+    expect(decodeUplink(input))
+    .toMatchObject({data: {
+      cmd: {
+        name: "CMD_GET_CABLE_CONNECTION",
+        id: 4,
+        success: true,
+        value: {
+          cable_position: "LEFT"
+        }
+      }
+    }})
+
+    input = {
+      fPort: fport,
+      bytes: [4, 0]
+    }
+
+    expect(decodeUplink(input))
+    .toMatchObject({data: {
+      cmd: {
+        name: "CMD_GET_CABLE_CONNECTION",
+        id: 4,
+        success: true,
+        value: {
+          cable_position: "RIGHT"
+        }
+      }
+    }})
+
   })
 })
 
